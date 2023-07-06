f6ff10f1a82b6b0811b75a770f474d0c5f0ceab8
tiboo2000 <91387403+tiboo2000@users.noreply.github.com>
2023-07-06 21:32:39 +0200

    Add files via upload

---
+++ choix_planète/script_saturne.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('#submitButton').click(function() {
+    var inputVal = $('#inputValue').val();
+
+    if (inputVal.length !== 4) {
+      // Afficher une alerte si l'entrée n'a pas 4 caractères
+      alert("Entrer 4 chiffres !");
+    } else {
+      // Rediriger vers la page Jupiter avec la valeur de l'entrée comme paramètre
+      window.location.href = '../planete/saturne.html?inputValue=' + inputVal;
+    }
+  });
+});
---
+++ planete/script_mercure.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+  var urlParams = new URLSearchParams(window.location.search);
+  var inputValue = urlParams.get('inputValue');
+  console.log(inputValue); // Affiche la valeur du paramètre dans la console
+
+  $('.button').click(function() {
+    if ($(this).attr('id') !== 'compareButton') {
+      var value = parseInt($(this).text(), 10);
+      value = (value + 1) % 10;
+      $(this).text(value);
+    }
+  });
+
+  $('#compareButton').click(function() {
+    var inputArray = inputValue.split(''); // Convertit la valeur de l'input en un tableau de chiffres
+    var buttonArray = $('.button').toArray(); // Convertit les boutons en un tableau d'éléments
+
+    var isMatch = true;
+
+    // Vérifier si chaque bouton a la même valeur que le chiffre correspondant de l'URL
+    for (var i = 0; i < inputArray.length; i++) {
+      var inputDigit = parseInt(inputArray[i]);
+      var buttonDigit = parseInt($(buttonArray[i]).text());
+
+      if (inputDigit !== buttonDigit) {
+        isMatch = false;
+        break;
+      }
+    }
+
+    if (isMatch) {
+      // Rediriger vers une autre page
+      window.location.replace("../affichage_planete/mercure.html");
+    } else {
+      // Mettre à jour les styles des boutons
+      for (var j = 0; j < buttonArray.length; j++) {
+        var inputDigit = parseInt(inputArray[j]);
+        var buttonDigit = parseInt($(buttonArray[j]).text());
+
+        if (inputDigit === buttonDigit) {
+          $(buttonArray[j]).css('background-color', 'green');
+        } else {
+          $(buttonArray[j]).css('background-color', 'red');
+        }
+      }
+    }
+  });
+});
